92dfb236709f465dff7d8493a507c5c1bd173bf1
Shist <shist.pupust@mail.ru>
2024-05-07 09:20:42 +0300

    Added prettier file with project format settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,13 +13,10 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
+    "prettier/prettier": "error",
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "auto",
-      },
-    ],
+    "css.lint.unknownAtRules": 0,
+    "linebreak-style": 0,
   },
 };
