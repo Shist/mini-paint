217adfca23d55feab8a51eb9e7366a4834e49853
Shist <shist.pupust@mail.ru>
2024-05-10 14:41:06 +0300

    Added setting for Vue Router that will always set scroll-top to 0

--- src/router/index.ts
+++ src/router/index.ts
@@ -37,6 +37,9 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  scrollBehavior() {
+    return { top: 0 };
+  },
 });
 
 router.beforeEach((to, from, next) => {
